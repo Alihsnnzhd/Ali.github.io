74a82041d19f4a0510626be30fb4bac47ec5e5bd
Ali hsnnzhd <alihassannegad44@gmail.com>
2025-12-16 01:21:21 +0330

    Feat: Fix Icons

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { translations } from './constants';
 import { Translation, Language } from './types';
 import Sidebar from './components/Sidebar';
-import { Github, Instagram, Mail, Smartphone, Code2, Database, Layout, Terminal, ExternalLink, Download, Globe, ArrowUpRight, Send, MapPin, Linkedin } from 'lucide-react';
+import { Github, Instagram, Mail, Smartphone, Code2, Database, Layout, Terminal, ExternalLink, Download, Globe, ArrowUpRight, Send, MapPin, Linkedin, PictureInPicture, Image, LogsIcon, Icon, AxeIcon, UserX, AppWindow, PcCase } from 'lucide-react';
 
 // --- Sub Components ---
 
@@ -436,7 +436,7 @@ const App: React.FC = () => {
                   <div className="bg-[#111] border border-white/5 rounded-[40px] p-10 hover:border-neon/30 transition-all duration-500">
                     <h3 className="text-2xl font-bold text-white mb-8 flex items-center gap-4">
                       <div className="p-3 bg-neon/10 rounded-xl text-neon"><Globe size={28} /></div>
-                      {t.skills.categories.Design}
+                      {t.skills.categories.web}
                     </h3>
                     <SkillBar name="HTML & CSS" percentage={80} icon={<Layout size={16}/>} />
                     <SkillBar name="Javascript" percentage={20} icon={<Code2 size={16}/>} />
@@ -446,12 +446,12 @@ const App: React.FC = () => {
                <div className="space-y-10">
                   <div className="bg-[#111] border border-white/5 rounded-[40px] p-10 hover:border-neon/30 transition-all duration-500">
                     <h3 className="text-2xl font-bold text-white mb-8 flex items-center gap-4">
-                      <div className="p-3 bg-neon/10 rounded-xl text-neon"><Globe size={28} /></div>
-                      {t.skills.categories.web}
+                      <div className="p-3 bg-neon/10 rounded-xl text-neon"><PcCase size={28} /></div>
+                      {t.skills.categories.Design}
                     </h3>
-                    <SkillBar name="Photoshop" percentage={95} icon={<Layout size={16}/>} />
-                    <SkillBar name="Illustrator" percentage={85} icon={<Code2 size={16}/>} />
-                    <SkillBar name="Adobe XD" percentage={80} icon={<Code2 size={16}/>} />
+                    <SkillBar name="Photoshop" percentage={95} icon={<Image size={16}/>} />
+                    <SkillBar name="Illustrator" percentage={85} icon={<AxeIcon size={16}/>} />
+                    <SkillBar name="Adobe XD" percentage={80} icon={<AppWindow size={16}/>} />
                   </div>
                </div>
             </div>
