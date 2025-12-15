cb9c746c2834443bec28a812b4e8ac86f97d7759
Ali hsnnzhd <alihassannegad44@gmail.com>
2025-12-15 23:29:47 +0330

    Fix: Correct homepage URL and base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Ali.github.io/', 
+  base: '/', 
   plugins: [react()],
 })
 
