c256171a79fdab8cb5856d7a6baf6da4ce081b79
Ali hsnnzhd <alihassannegad44@gmail.com>
2025-12-15 23:46:40 +0330

    Final config check for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', 
+  base: '/', // <-- باید فقط یک اسلش باشد
   plugins: [react()],
 })
 
